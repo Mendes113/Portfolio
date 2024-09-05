dbe95ee9f1a03edc0b864efe4206d8296a8a660d
Mendes113 <andremendes0113@gmail.com>
2024-09-05 16:56:27 -0300

    style: fix cards and buttons

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -6,16 +6,19 @@ import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
-    <Card className="project-card-view">
+    <Card className="project-card-view"  >
       <Card.Img variant="top" src={props.imgPath} alt="card-img"
         style={{ width: '100%', height: '300px'}}
       />
-      <Card.Body>
+      <Card.Body className="d-flex flex-column justify-content-between ">
+        <div>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
+        </div>
+      <div className="mt-3">
+        <Button variant="primary" href={props.ghLink} target="_blank" className="align-self-center">
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
         </Button>
@@ -35,6 +38,7 @@ function ProjectCards(props) {
             {"Demo"}
           </Button>
         )}
+        </div>
       </Card.Body>
     </Card>
   );
